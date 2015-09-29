963558e14959d773ece186d876ce96e1fe9550fe
Stacy Andrews <stacy.andrews@iinet.net.au>
2015-09-30 07:31:18 +0800

    first bsearch implementation complete

--- lib/karate_chop.rb
+++ lib/karate_chop.rb
@@ -1,7 +1,8 @@
 require "karate_chop/version"
+require 'byebug'
 
 module KarateChop
-  def self.chop(int, array_of_int)
+  def self.chop(int, array_of_int, start_index = 0)
     if array_of_int.empty?
       return -1
     end
@@ -9,25 +10,22 @@ module KarateChop
     middle_index = array_of_int.length/2
     middle_value = array_of_int[middle_index]
 
-    puts "#{middle_index} - #{middle_value}"
-
     if middle_value == int
-      return middle_index
+      return middle_index + start_index
     end
 
     if int < middle_value
       new_array = array_of_int.take(middle_index)
 
       if new_array.length == 1
-
         if new_array[0] === int
-          return middle_index - 1
+          return (middle_index - 1) + start_index
         else
           return -1
         end
       end
 
-      puts new_array
+      return chop(int, new_array, start_index)
     end
 
     if int > middle_value
@@ -35,19 +33,21 @@ module KarateChop
 
       if new_array.length == 1
         if new_array[0] === int
-          return middle_index - 1
+          return (middle_index - 1) + start_index
         else
           return -1
         end
       end
+
+      return chop(int, new_array, middle_index)
     end
 
-    check int, array_of_int, 0
+    check int, array_of_int, start_index
   end
 
-  def check(int, array_of_int, index)
+  def check(int, array_of_int, start_index)
     if array_of_int.length == 1 && array_of_int[0] === int
-      return index
+      return index + start_index
     end
 
     -1
@@ -59,9 +59,40 @@ module KarateChop
     n + sum(n - 1)
   end
 
-  def self.min(array_of_int, length)
-    item = array_of_int[0]
+  def self.min(min_value = nil, array_of_int)
+    return min_value if array_of_int.length == 0
 
+    new_min = array_of_int[0]
+
+    if min_value.nil? || new_min < min_value
+      return min new_min, array_of_int.drop(1)
+    end
     
+    min min_value, array_of_int.drop(1)
+  end
+
+  def self.sum_all(array_of_int)
+    return nil if array_of_int.length == 0
+
+    return array_of_int[0] if array_of_int.length == 1
+
+    array_of_int[0] + sum_all(array_of_int.drop(1))
+  end
+
+  def self.palindrome?(array_of_chars)
+    return true if array_of_chars.length == 1    
+    
+    could_be = array_of_chars.first == array_of_chars.last
+
+    if !could_be
+      return false 
+    else
+      return true if array_of_chars.length == 2
+
+      array_of_chars.delete_at(0)
+      array_of_chars.delete_at(array_of_chars.length-1)
+
+      return palindrome?(array_of_chars)
+    end
   end
 end
--- spec/karate_chop_spec.rb
+++ spec/karate_chop_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 require 'karate_chop'
 
 RSpec.describe KarateChop do
-  it "index of via chop" do
+  it 'index of with binary search' do
     expect(KarateChop.chop(3, [])).to eq(-1)
     expect(KarateChop.chop(3, [1])).to eq(-1)
     expect(KarateChop.chop(1, [1])).to eq(0)
 
     expect(KarateChop.chop(1, [1, 3, 5])).to eq(0)
     expect(KarateChop.chop(3, [1, 3, 5])).to eq(1)
-    # expect(KarateChop.chop(5, [1, 3, 5])).to eq(2)
-    # expect(KarateChop.chop(0, [1, 3, 5])).to eq(-1)
-    # expect(KarateChop.chop(2, [1, 3, 5])).to eq(-1)
-    # expect(KarateChop.chop(4, [1, 3, 5])).to eq(-1)
-    # expect(KarateChop.chop(6, [1, 3, 5])).to eq(-1)
+    expect(KarateChop.chop(5, [1, 3, 5])).to eq(2)
+    expect(KarateChop.chop(0, [1, 3, 5])).to eq(-1)
+    expect(KarateChop.chop(2, [1, 3, 5])).to eq(-1)
+    expect(KarateChop.chop(4, [1, 3, 5])).to eq(-1)
+    expect(KarateChop.chop(6, [1, 3, 5])).to eq(-1)
     # #
-    # expect(KarateChop.chop(1, [1, 3, 5, 7])).to eq(0)
-    # expect(KarateChop.chop(3, [1, 3, 5, 7])).to eq(1)
-    # expect(KarateChop.chop(5, [1, 3, 5, 7])).to eq(2)
-    # expect(KarateChop.chop(7, [1, 3, 5, 7])).to eq(3)
+    expect(KarateChop.chop(1, [1, 3, 5, 7])).to eq(0)
+    expect(KarateChop.chop(3, [1, 3, 5, 7])).to eq(1)
+    expect(KarateChop.chop(5, [1, 3, 5, 7])).to eq(2)
+    expect(KarateChop.chop(7, [1, 3, 5, 7])).to eq(3)
     # expect(KarateChop.chop(0, [1, 3, 5, 7])).to eq(-1)
     # expect(KarateChop.chop(2, [1, 3, 5, 7])).to eq(-1)
     # expect(KarateChop.chop(4, [1, 3, 5, 7])).to eq(-1)
@@ -26,14 +26,35 @@ RSpec.describe KarateChop do
     # expect(KarateChop.chop(8, [1, 3, 5, 7])).to eq(-1)
   end
 
-  it "sum to X" do
+  it 'sum to X' do
     expect(KarateChop.sum(5)).to eq(5+4+3+2+1)
   end
 
   it 'finds a minimum' do
-    expect(KarateChop.min([2, 4, 5], 3)).to eq(2);
+    expect(KarateChop.min([2, 4, 5])).to eq(2);
 
-    expect(KarateChop.min([5, 4, 2], 3)).to eq(2);
+    expect(KarateChop.min([5, 4, 2])).to eq(2);
 
+    expect(KarateChop.min([])).to be_nil 
   end
+
+  it 'adds up all elements' do
+    expect(KarateChop.sum_all([2, 5, 4])).to eq(11)
+
+    expect(KarateChop.sum_all([])).to be_nil
+  end
+
+  it 'determines if is a palindrome' do
+    expect(KarateChop.palindrome?(['a'])).to eq(true)
+    
+    expect(KarateChop.palindrome?(['a', 'a'])).to eq(true)
+    
+    expect(KarateChop.palindrome?(['a', 'b'])).to eq(false)
+
+    expect(KarateChop.palindrome?(['a', 'b', 'a'])).to eq(true)
+
+    expect(KarateChop.palindrome?(['a', 'b', 'c'])).to eq(false)
+  end
+
+
 end
